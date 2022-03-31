bc6e40b97b731acdaa09981e02801d0c8e09abb6
cenlb <82813986+cenlb@users.noreply.github.com>
2022-03-31 15:40:38 +0100

    Update Chen_2020.py

--- values_and_distributions/Chen_2020.py
+++ values_and_distributions/Chen_2020.py
@@ -1,6 +1,10 @@
 import math as m
 '''
-Chen2020 Fig A1(b)
+From:
+Chen, W., Zhang, N., Wei, J., et al.
+Short-range airborne route dominates exposure of respiratory infection during close contact (2020)
+
+Figures taken from Fig. A1(b)
 '''
 distance = [0.1, 0.5, 1.0, 1.5, 2.0] #metres
 log_10_eSR = [-1.62, -3.86, -5.22, -6.05, -6.60]
